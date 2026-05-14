2e0a5edd7ea0e7bc96cc83e5b7ad2fb481bad478
Dante Accinelli <Dante.Primon@t-systems.com>
2026-05-14 11:17:10 -0300

    feat: add emg1 comments

--- Emg1_BurstAndPlot.py
+++ Emg1_BurstAndPlot.py
@@ -1,28 +1,63 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 14 17:44:54 2023
+# Emg script 1/4, Burst And Plot.
 
-@author: kerberos
+"""
+This script is mainly used to synthetically generate an EMG signal.
+It does that by concatenating quiet and active segments (resting and burst muscle activity),
+then plots and saves the result as an image. There is no real sensor data,
+it is purely for demonstration or testing purposes.
 """
 
+# Here, we import two heavily important python libraries, numpy and matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-#matplotlib inline
 
-# simulate EMG signal
-burst1 = np.random.uniform(-1, 1, size=1000) + 0.08
-burst2 = np.random.uniform(-1, 1, size=1000) + 0.08
-quiet = np.random.uniform(-0.05, 0.05, size=500) + 0.08
-emg = np.concatenate([quiet, burst1, quiet, burst2, quiet])
-time = np.array([i/1000 for i in range(0, len(emg), 1)]) # sampling rate 1000 Hz
+"""
+Imports numpy and creates an alias for it, so every time we call "np", we are calling numpy lib.
+The same applies to matplotlib as plt.
+Numpy is a library that adds support for large and multi-dimensional arrays and matrices,
+and a large collection of high-level mathematical functions to operate on them.
+Matplotlib, as the name suggests, is a plotting library and it is a numerical Numpy extension.
+"""
 
-# plot EMG signal
-fig = plt.figure()
-plt.plot(time, emg)
+# Here, we simulate all the EMGs signals that we are going to plot and use
+dc_offset = 0.08
+active_segment_1 = np.random.uniform(-1, 1, size=1000) + dc_offset
+active_segment_2 = np.random.uniform(-1, 1, size=1000) + dc_offset
+rest_segment = np.random.uniform(-0.05, 0.05, size=500) + dc_offset
+emg_signal = np.concatenate([rest_segment, active_segment_1, rest_segment, active_segment_2, rest_segment])
+
+"""
+np.random.uniform(low, high, size) is a command that generates "size" random samples,
+distributed between low and high numbers. So for burst1 and 2, we are asking for 1000 samples,
+between -1 and 1. The + 0.08 constant is a baseline to every sample, simulating a small noise
+and prevent it to be zero, like real-world muscles.  
+quiet variable asks for the same thing, but it wants samples between -0.05 and 0.05, 
+simulating a resting muscle without any movement.
+
+The emg array is assembled to simulate a muscle: 
+rest -> burst1 -> rest -> burst2 -> rest
+"""
+# Here, we have time axis
+sampling_rate = 1000 # Hz
+time_vector = np.arange(len(emg_signal)) / sampling_rate
+
+"""
+This builds a time vector that corresponds to each sample in the signal.
+The sampling rate 1000 means that 1000 samples are recorded per second, so the
+time interval of each sample is 1/1000 = 0.001 seconds. It has the same length as emg_signal,
+where each value represents time axis.
+"""
+
+# Here, we plot and save the information
+plot_filename = 'BurstAndPlotGraph.png'
+figure = plt.figure()
+plt.plot(time_vector, emg_signal)
 plt.xlabel('Time (sec)')
 plt.ylabel('EMG (a.u.)')
-fig_name = 'fig2.png'
-fig.set_size_inches(w=11,h=7)
-fig.savefig(fig_name)
+figure.set_size_inches(w=11,h=7)
+figure.savefig(plot_filename)
 
+"""
+This defines the name of the image and plots time_vector as x and emg_signal as y.
+On x label it gives the "Time (sec)" name, and in y label "EMG (a.u.)", arbitrary units.
+"""
